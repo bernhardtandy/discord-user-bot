b726b54fe067d271c180cb52c2a27f87bce30013
bernhardtandy <bernha@rpi.edu>
2020-12-21 20:17:53 -0500

    Modified the bot to work with multiple servers

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+from os import path
 
 import discord
 from dotenv import load_dotenv
@@ -11,20 +12,31 @@ GUILD = os.getenv('DISCORD_GUILD')
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
-mc = MarkovChain("bot_data.txt")
+markovChainsDict = {}
+#mc = MarkovChain("bot_data.txt")
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+	for guild in client.guilds:
+		print(f'{client.user} is connected to {guild.name}\n')
+		if (path.exists("bot_data_" + guild.name + ".txt")):
+			markovChainsDict[guild.name] = MarkovChain("bot_data_" + guild.name + ".txt")
+		else:
+			file = open("bot_data_" + guild.name + ".txt", "w")
+			file.write(str(0) + "\n" + str(0) + "\n" + str(0))
+			file.close()
+			markovChainsDict[guild.name] = MarkovChain("bot_data_" + guild.name + ".txt")
+	print(markovChainsDict)
+    # for guild in client.guilds:
+    #     if guild.name == GUILD:
+    #         break
 
-    print(
-        f'{client.user} is connected to {guild.name}\n'
-    )
+    # print(
+    #     f'{client.user} is connected to {guild.name}\n'
+    # )
 
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Server Members:\n - {members}')
+    # members = '\n - '.join([member.name for member in guild.members])
+    # print(f'Server Members:\n - {members}')
 
 @client.event
 async def on_message(message):
@@ -32,15 +44,22 @@ async def on_message(message):
 		return
 
 	if (not(message.content.startswith("!"))):
-		mc.incrementMessageCount()
-		mc.updateModel(message.content)
-		mc.saveToFile("bot_data.txt")
+		markovChainsDict[message.guild.name].incrementMessageCount()
+		markovChainsDict[message.guild.name].updateModel(message.content)
+		markovChainsDict[message.guild.name].saveToFile("bot_data_" + message.guild.name + ".txt")
+		# mc.incrementMessageCount()
+		# mc.updateModel(message.content)
+		# mc.saveToFile("bot_data.txt")
 
 	if (message.content.startswith("!speak")):
-		response = mc.constructSequence(int(message.content.split()[1]))
+		if (int(message.content.split()[1]) > 3):
+			response = markovChainsDict[message.guild.name].constructSequence(int(message.content.split()[1]))
+		else: response = markovChainsDict[message.guild.name].constructSequence(10)
+		#response = mc.constructSequence(int(message.content.split()[1]))
 		await message.channel.send(response)
-	elif (mc.getMessageCount() % 20 == 0):
-		response = mc.constructSequence(20)
+	elif (markovChainsDict[message.guild.name].getMessageCount() % 20 == 0):
+		#response = mc.constructSequence(20)
+		response = markovChainsDict[message.guild.name].constructSequence(20)
 		await message.channel.send(response)
 
 client.run(TOKEN)
